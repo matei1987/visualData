22c545d1a6e0956803014893409fdde19ee83009
Chris Bras <ceb922@hotmail.com>
2013-03-24 15:27:13 -0400

    added data json to index.html changed case of statecodes and made extrusion random

--- map/index.js
+++ map/index.js
@@ -1,10 +1,13 @@
-
+  var dataJSON = {
+    'pensions': pensions,
+    'education': education,
+    'healthcare': healthcare
+  };
   var colorArray = [10571751.13940574, 3173614.0569316, 9563453.703411205, 13566412.191378772, 3205732.246423457, 510009.23913232866, 5553613.829135051, 9853254.740825217, 4491309.27526584, 11631465.65436683, 14945459.609181136, 8099660.212535102, 14592825.958324742, 9283951.76694582, 2923730.6421383135, 11666026.488244344, 1067065.034054214, 2057363.4906528227, 12416542.963041324, 2390840.056713569, 13317874.108536547, 13024219.977602199, 9886527.488842007, 8380292.230964378, 2406184.8057989506, 5314210.120748375, 3310143.044887588, 14470882.992936859, 7965356.12679025, 11550910.70213703, 422745.1466774242, 3047906.9316118294, 10708356.45548218, 11858674.148636648, 14571121.93227465, 7942466.29612334, 4917391.163931879, 11321339.171336222, 3528461.106093567, 767573.5167490505, 8130699.398185022, 12086219.138980158, 11746055.338943003, 16774037.89472062, 9088119.919243308, 5511252.984003704, 15653468.727136752, 11367309.865424244, 13724763.025690326, 14044767.268335337, 13830761.956872297];
 
 
   var states = new THREE.Object3D();
 function init() {
-  clock = new THREE.Clock();
   
   container = document.getElementById('container');
   containerWidth = container.clientWidth,
@@ -41,10 +44,6 @@ function init() {
     i++;
   }
 
-  var light = new THREE.PointLight(parseInt('#FFFFFF'.replace('#', '0x')));
-  light.position.set(10, 13, 7);
-  scene.add(light);
-
 
   var projecter = new THREE.Projector(),
   mouseVector = new THREE.Vector3();
@@ -56,7 +55,17 @@ function init() {
   controls.rollSpeed = Math.PI / 12;;
   controls.autoForward = false;
   controls.dragToLook = true;
-  
+ 
+
+  $('a').click(function(){
+    extrudeStates($(this).attr('id'));
+  });
+
+function extrudeStates(data){
+  console.log(dataJSON[data]);  
+
+}
+
 function onMouseMove (e){
   var position = $('canvas').position(),
   offsetY = position.top,
@@ -83,22 +92,20 @@ function onMouseMove (e){
 function animate() {
   
   requestAnimationFrame(animate);
+  controls.update();
   renderer.render(scene, camera);
-  
-  var delta = clock.getDelta();
-  controls.update(delta);
 };
 
 function createShape( shape, color, x, y, z, rx, ry, rz, s, xx ) {
-  
-  var eGeom = new THREE.ExtrudeGeometry( shape, {amount: 5 * xx , bevelEnabled: false } );
+  var amount = Math.random() * (500 - 10) + 10; 
+  var eGeom = new THREE.ExtrudeGeometry( shape, {amount: amount, bevelEnabled: false } );
   var material = new THREE.MeshBasicMaterial({
     color: color 
   });
 
   var mesh = new THREE.Mesh( eGeom, material );
   s = 2;
-  mesh.position.set( x-window.innerWidth/(2), y+window.innerHeight/(2), z + 10 * xx );
+  mesh.position.set( x-window.innerWidth/(2), y+window.innerHeight/(2), z + (2 * amount));
   mesh.rotation.set( rx, ry, rz );
 
   mesh.scale.set( s, s, s );
@@ -347,5 +354,6 @@ $(document).ready(function() {
     
     init();
     animate();
+
 
 });
