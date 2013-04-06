26ada55e52583767f8d892b1c691f19e946bee51
Chris Bras <ceb922@hotmail.com>
2013-04-06 14:55:19 -0400

    wtf

--- map/index.js
+++ map/index.js
@@ -187,7 +187,7 @@ Map.Views.App = Backbone.View.extend({
       $("#title").text(stateModel.get('name'));
       $("#local").text("Local: "+hoverData.local);
       $("#state").text("State: "+hoverData.state);
-      $("#total").text("Total: "+ (hoverData.state + hoverData.local));
+      $("#total").text("Total: "+ (0 + hoverData.state + hoverData.local));
 
       tip.show();
     }
